803cff46e28c59eba80497b97f597e5c587a5725
Igor Henrique Constant <igorhenriqueconstant@gmail.com>
2025-08-30 14:21:44 -0300

    first commit

---
+++ src/main/java/br/com/igor/lotr_api/LotrApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.igor.lotr_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igor/lotr_api/LotrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igor.lotr_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
